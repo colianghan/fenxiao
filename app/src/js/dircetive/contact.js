8087e2efb958b840d698aad0c23d3005eae2ea64
korealiang <hanguoliang1992@gmail.com>
2014-12-30 18:54:40 +0800

    2014-12-30

---
+++ app/src/js/dircetive/contact.js
@@ -0,0 +1,162 @@
+dm.directive('concat',['$rootScope',function($rootScope){
+	var compile = function($element,$attrs,link){
+		return function($scope,$element,$attrs,link){
+			console.log('concat');
+		}
+	};
+	var control = ['$rootScope','$scope','$element','$attrs','$parse','$timeout','concatModel',function($rootScope,$scope,$element,$attrs,$parse,$timeout,concatModel){
+		//debugger;
+		//var canSave = true; //页面没有错误
+		$scope.errorMessage = '';//错误消息
+		var model = $scope.model = $parse($attrs.a)($scope.$parent)||{};//获取用户东西
+		if($scope.model===undefined){
+			console.log('添加记录');
+		}
+		console.log('展示记录'+$scope.model);
+		_.each(concatModel,function(item,key){
+			item.key = key;
+		});
+		var keys = _.keys(concatModel);
+		var values = _.values(concatModel);
+		//var attrSrc = $scope.attrSrc = concatModel;
+		var attrs= $scope.attrs = values.slice(1,keys.length-1);
+		$scope.attrCheck = function(e,key){
+			var $ele = $(e.currentTarget).parents('.control-group');
+			if(concatModel[key].check){
+				var resp = concatModel[key].check(model[key]);
+				if(!resp.success){
+					//出错了
+					canSave = false;
+					$ele.addClass('error');
+					console.log(resp.message);
+					$scope.errorMessage = resp.message;
+					return;
+					/*$timeout(function(){
+						$scope.errorMessage='';
+					},200);*/
+				}
+			}
+			$ele.removeClass('error');
+			$scope.errorMessage = '';
+			//canSave = true;
+		};
+		$scope.remove = function(){
+			//移除
+			$rootScope.$broadcast('remove-concat',model);
+			//$element.remove();
+		};
+		$scope.save = function(){
+			//保存
+			$rootScope.$broadcast('save-concat',model);
+		};
+		$scope.edit = function(){
+			//编辑
+			$rootScope.$broadcast('edit-concat',model);
+		};
+	}];
+	return{
+		restrict:'E',
+		scope:true,
+		replace:true,
+		templateUrl:'../html/template/contact.html',
+		compile:compile,
+		controller:control
+	}
+}]);
+
+dm.factory('concatModel',['tools',function(tools){
+	var mail =/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+	var mobilePhone =/^[+]{0,1}(\d){1,3}[ ]?([-]?((\d)|[ ]){1,12})+$/;
+	var postcode = /^[1-9]\d{5}(?!\d)$/;
+	var url=/^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i;
+	var model = {
+		name:{
+			name:'名称',
+			check:function(v){
+				if(v!=undefined&&v!=''){
+					return {success:true};
+				}
+				return {success:false,message:'名称不能为空'};		
+			}
+		},
+		duty:{
+			name:'职务'
+		},
+		location:{
+			name:'地址'
+		},
+		postcode:{
+			name:'邮编',
+			check:function(v){
+				var _bool = {success:true};
+				if(v!=undefined&&v!=''&&!postcode.test(v)){
+					_bool.success = false;
+					_bool.message="邮编格式不正确";
+				}
+				return _bool;
+			}
+		},
+		shopUrl:{
+			name:'店铺地址',
+			check:function(v){
+				var _bool = {success:true};
+				if(v!=undefined&&v!=''&&!url.test(v)){
+					_bool.success = false;
+					_bool.message="店铺地址URl格式不正确";
+				}
+				return _bool;
+			}
+		},
+		mobilePhone:{
+			name:'手机号码',
+			check:function(v){
+				var _bool = {success:true};
+				if(v!=undefined&&v!=''&&!url.test(v)){
+					_bool.success = false;
+					_bool.message="手机号码格式不正确";
+				}
+				return _bool;
+			}
+		},
+		telphone:{
+			name:'联系电话'
+		},
+		aliNick:{
+			name:'旺旺'
+		},
+		qq:{
+			name:'QQ',
+			check:function(v){
+				var _bool = {success:true};
+				if(v!=undefined&&v!=''&&!/^\d{n}$/.test(v)){
+					_bool.success = false;
+					_bool.message="QQ格式不正确";
+				}
+				return _bool;
+			}
+		},
+		email:{
+			name:'E-MAIL',
+			check:function(v){
+				var _bool = {success:true};
+				if(v!=undefined&&v!=''&&!mail.test(v)){
+					_bool.success = false;
+					_bool.message="mail格式不正确";
+				}
+				return _bool;
+			}
+		},
+		remark:{
+			name:'备注',
+			check:function(v){
+				var _bool = {success:true};
+				if(v!=undefined&&v!=''&&!v.replace(/[\u4e00-\u9fa5]/gm,'**').length>400){
+					_bool.success = false;
+					_bool.message = "备注不能超过200字";
+				}
+				return _bool;
+			}
+		}
+	};
+	return model;
+}]);
--- app/src/js/dircetive/distributor-detail.js
+++ app/src/js/dircetive/distributor-detail.js
@@ -1,5 +1,5 @@
 dm.directive('distrdetail',['$rootScope','$compile',function($rootScope,$compile){
-	var control = ['$scope','$rootScope','$element','distrDetail',function($scope,$rootScope,$element,distrDetail){
+	var control = ['$scope','$rootScope','$element','distrDetail','initPages',function($scope,$rootScope,$element,distrDetail,initPages){
 		$scope.options = {};
 		/*
 		note : ng-if 中的事件监听 没有用 可以用$scope.$parent来获取
@@ -21,19 +21,64 @@ dm.directive('distrdetail',['$rootScope','$compile',function($rootScope,$compile
 		$scope.curNavIndex = 0;
 		$scope.setNav = function(index){
 			$scope.curNavIndex = index;
-		}
+			$scope.parms.begin = 0;
+			$scope.parms.end = false;
+			$scope.now=1;
+			$scope.data = null;
+			$scope.pages=null;
+			_obj[$scope.curNavIndex]();
+		};
 		$scope.thead=[
 			[ {name:'宝贝',key:'title',sortable:false},{name:'PV',key:'ipv',sortable:'IPV'},
 			  {name:'UV',key:'iuv',sortable:'IUV'},{name:'成交额',key:'alipayTradeAmt',sortable:'ALIPAY_TRADE_AMT'},
 			  {name:'成交量',key:'alipayAuctionNum',sortable:'ALIPAY_AUCTION_NUM'},{name:'转化率',key:'transformationEfficiency',sortable:'ALIPAY_WINNER_NUM/IUV'},
 			  {name:'停留时间',key:'pageDuration',sortable:'PAGE_DURATION/IUV'},{name:'跳失率',key:'lossRate',sortable:'BOUNCE_CNT/LANDING_CNT'},
 			  {name:'拍下未付款',key:'noPayNum',sortable:'NO_PAY_NUM'},{name:'加入购物车',key:'addCartUserNum',sortable:'ADD_CART_USER_NUM'}
+			],
+			[{name:'关键词',key:'query',sortable:false},{name:'访客数',key:'uv',sortable:'UV'},{name:'成交额',key:'alipayTradeAmt',sortable:'ALIPAY_TRADE_AMT'},
+			 {name:'成交用户数',key:'alipayAuctionNum',sortable:'ALIPAY_AUCTION_NUM'},{name:'客单价',key:'perCustomerTransaction',sortable:'SUM(ALIPAY_TRADE_AMT)/SUM(ALIPAY_WINNER_NUM)'},{name:'转化率',key:'transformationEfficiency',sortable:'SUM(ALIPAY_WINNER_NUM)/SUM(UV)'},
+			 {name:'展现量',key:'impression',sortable:'IMPRESSION'},{name:'点击率',key:'clickRate',sortable:'SUM(CLICK)/SUM(IMPRESSION)'}
+			],
+			[{name:'PV',key:'ipv',sortable:'IPV'},{name:'UV',key:'iuv',sortable:'IUV'},{name:'成交额',key:'alipayTradeAmt',sortable:'ALIPAY_TRADE_AMT'},
+			 {name:'成交量',key:'alipayAuctionNum',sortable:'ALIPAY_AUCTION_NUM'},{name:'订单数',key:'alipayTradeNum',sortable:'ALIPAY_TRADE_NUM'},{name:'转化率',key:'transformationEfficiency',sortable:'SUM(ALIPAY_WINNER_NUM)/SUM(IUV)'},
+			 {name:'客单价',key:'perCustomerTransaction',sortable:'SUM(ALIPAY_TRADE_AMT)/SUM(ALIPAY_WINNER_NUM)'}
 			]
 		];
-		$scope.pages=[0,1,2,3];
-		$scope.getPage = function(){
-			alert('distrdetail');
+		//$scope.pages=[0,1,2,3];
+		$scope.getPage = function(page){
+			//var begin = 
+			if($scope.now == page){
+				return;
+			}
+			$scope.now=page;
+			var count = $scope.parms.tableLength,
+				begin = (page-1)*count,
+				end = page*count-1;
+			//$scope.parms.search(newParms);
+			//alert('distrdetail');
+			$scope.parms.begin = begin;
+			$scope.parms.end = end;
+			_obj[$scope.curNavIndex]();
 		}
+		$scope.sort = function(e,item){
+			if(!item.sortable){
+				return;
+			}
+			var $ele = $(e.currentTarget),sortBy;
+			if($ele.hasClass('sort-desc')){
+				//升序
+				$ele.removeClass('sort-desc').addClass('sort-asc').siblings().removeClass('sort-asc sort-desc');
+				sortBy='up';		
+			}else{
+				//降序
+				$ele.removeClass('sort-asc').addClass('sort-desc').siblings().removeClass('sort-asc sort-desc');
+				sortBy='down';
+			};
+			$scope.parms.sortBy=sortBy;
+			$scope.parms.sortColumn = item.sortable;
+			//$scope.parms.search(newParms);
+			_obj[$scope.curNavIndex]();
+		};
 		$scope.setLevel=function(leve){
 			if(isNaN(Number(leve))){
 				return;
@@ -49,12 +94,94 @@ dm.directive('distrdetail',['$rootScope','$compile',function($rootScope,$compile
 			distr.warning = !distr.warning;
 			$scope.parms.updateDistributorOfInfo();
 		};
-		
-
-
-
-
-
+		var _obj = {
+			0:function(){
+				//运营基础数据
+				//debugger;
+				$scope.parms.lowThedate = $('.lowThedate').val()||'2014-11-29';
+				$scope.parms.highThedate = $('.highThedate').val()||'2014-12-19';
+				$scope.parms.getOperateBasicData(function(v){
+					$scope.data = $scope.parms.operateBasicData;
+					if(v.resultSize){
+						initPages($scope,v.resultSize);
+					}
+				});
+				$scope.shopAttr = ['ipv','iuv','alipayTradeAmt','alipayAuctionNum','transformationEfficiency',
+					'noPayNum','addCartUserNum','pageDuration','lossRate'
+				];
+			},
+			1:function(){
+				$scope.shopAttr = ['click','uv','alipayTradeAmt','alipayAuctionNum','transformationEfficiency'];
+				$scope.parms.lowThedate = $('.lowThedate').val()||'2014-11-29';
+				$scope.parms.highThedate = $('.highThedate').val()||'2014-12-19';
+				$scope.parms.getKeyWordInfo();
+				$scope.parms.getKeyWordList(function(v){
+					$scope.data = $scope.parms.keyword.list;
+					if(v.resultSize){
+						initPages($scope,v.resultSize);
+					}
+					console.log($scope.parms.keyword);
+				});
+			},
+			2:function(){
+				$scope.parms.getDistriDataSrc(function(resp){
+					//debugger;
+					console.log($scope.parms.distriDataSrc);
+				});
+			},
+			3:function(){
+				//产品授权及折扣
+				$scope.parms.getProds(function(v){
+					debugger;
+					if(v.resultSize){
+						initPages($scope,v.resultSize);
+					}
+				});
+			},
+			4:function(){
+				$scope.parms.getRecordList(function(v){
+					//$scope.parms.concats=v.disLinkManInfos;
+					/*for(var i =0;i<=10;i++){
+						$scope.parms.concats.unshift({});
+					}*/
+				});
+				//console.log();
+				$scope.$on('remove-concat',function(e,v){
+					if(!v){
+						return;
+					}
+					/*if(!v.id){
+						$scope.parms.concats.shift();
+					}*/
+					$scope.parms.deleteConcat(v);
+					debugger;
+				});
+				$scope.$on('save-concat',function(e,v){
+					$scope.parms.addConcat(v);
+				});
+				$scope.$on('edit-concat',function(e,v){
+					$scope.parms.alterConcat(v);
+				});
+			},
+			5:function(){
+				$scope.parms.getRecordList();
+			},
+			6:function(){
+				$scope.parms.getRecordList();
+				console.log('店铺基本信息');
+			}
+		};
+		_obj[$scope.curNavIndex]();
+		$scope.getData = function(){
+			_obj[$scope.curNavIndex]();
+		};
+		$scope.editCount = function(i){
+			var value = $('#pricCount_'+i).val();
+			$scope.parms.editCount(i,value);
+		};
+		$scope.addConcat =function(){
+			$scope.parms.concats.unshift({});
+		};
 	}];
 	var compile = function(element,attrs,link){
 		return function(scope,element,attrs){
@@ -75,40 +202,74 @@ dm.directive('distrdetail',['$rootScope','$compile',function($rootScope,$compile
 dm.factory('distrDetail',['$rootScope','tools',function($rootScope,tools){
 	var api = {
 		getOperateBasicData:'getOperateBasicData.htm',
-		updateDistributorOfInfo:'updateDistributorOfInfo.htm'
-	}
+		updateDistributorOfInfo:'updateDistributorOfInfo.htm',
+		getDistributorDetailPvSrcEffectsByShop:'getDistributorDetailPvSrcEffectsByShop.htm',
+		getDistributorDetailQueryEffectsByQuery:'getDistributorDetailQueryEffectsByQuery.htm',/*获取汇总数据*/
+		getDistributorQueryEffectsDetailByQuery:'getDistributorQueryEffectsDetailByQuery.htm',/*获取单个详情*/
+		getDistributorDetailOfGatherQueryEffects:'getDistributorDetailOfGatherQueryEffects.htm',/*获取关键词 母店和分销的数据*/
+		getPrudsList:'getProductLineOfDistributorPurchasePriceList.htm',//获取膜分销商的授权产品信息
+		updatePrice:'updateDisPruchasePriceByPids.htm',//修改分销商的折扣
+		getRecordList:'getDistributorDetailContactInfo.htm', //获取联系记录列表
+		insertRecordList:'addDisContactHistoryRecord.htm', //添加联系记录
+		addConcat:'addDisLinkManInfo.htm',
+		updateConcat:'updateDisLinkManInfo.htm',
+		deleteConcat:'deleteDisLinkManInfo.htm'
+	};
 	var action = function(item){
 		var self = this;
-		this.gradId = item.gradId;
+		this.tableLength = tools.config.table.count;
+		this.disId = item.disShopId;
 		this.tradeType = item.tradeType;
-		this.operateBasicData = [];
+		this.operateBasicData = [];//运营基础数据
+		this.shopData = {}; // 对象 店铺概况
+		this.distriDataSrc=[];//流量渠道数据
+		this.keyword ={};//进店词  (shop:{},list:[])
 		this.highThedate = '';
 		this.lowThedate = '';
-		this.getOperateBasicData = function(){
-			var getAjax =  tools.promise(api.getOperateBasicData);
+		/*this.parms = {
+			disSid:this.disId,
+			tradeType : this.tradeType,
+			lowThedate: this.lowThedate,
+			highThedate:this.highThedate,
+			sort:this.sortBy,
+			sortColumn:this.sortKey
+		};*/
+		/*运营基础数据*/
+		this.getOperateBasicData = function(callback){
+			var getAjax =  tools.promise(api.getOperateBasicData,true);
 			getAjax({
 				data:{
-					disSid:this.gradId,
+					disSid:this.disId,
 					tradeType : this.tradeType,
 					lowThedate: this.lowThedate,
-					highThedate:this.highThedate
+					highThedate:this.highThedate,
+					sort:this.sortBy,
+					sortColumn:this.sortColumn,
+					begin:this.begin||0,
+					end:this.end||this.tableLength-1
 				}
 			}).then(function(resp){
 				if(resp.success){
 					//alert('成功');
 					self.operateBasicData = resp.value.auctionData;
-					console.log(resp.value);
-					debugger;
+					self.shopData = resp.value.shopData;
+					//console.log(resp.value);
+					if(callback){
+						callback(resp.value);
+					}
+				}else{
+					alert(resp.message);
 				}
 			});	
 		};
+		/*更新用户状态*/
 		this.updateDistributorOfInfo = function(){
 			console.log(item.warning);
 			console.log(item.assessmentLevel);
 			tools.http({
 				url:api.updateDistributorOfInfo,
 				data:{
-					disSid:this.gradId,
+					disSid:this.disId,
 					warning:item.warning,
 					payAttention:item.payAttention,
 					assessmentLevel:item.assessmentLevel,
@@ -120,8 +281,261 @@ dm.factory('distrDetail',['$rootScope','tools',function($rootScope,tools){
 					}
 				}
 			});
+		};
+		/*进店词数据*/
+		this.getKeyWordInfo = function(callback){
+			var getAjax = tools.promise(api.getDistributorDetailOfGatherQueryEffects,true);
+			getAjax({
+				data:{
+					disSid:this.disId
+				}
+			}).then(function(resp){
+				if(resp.success){
+					self.keyword.shop = resp.value;
+					//debugger;
+					if(callback){
+						callback(resp.value);
+					}
+				}else{
+					alert(resp.message);
+				}
+			});
+		};
+		this.getKeyWordList = function(callback){
+			var getAjax = tools.promise(api.getDistributorDetailQueryEffectsByQuery,true);
+			getAjax({
+				data:{
+					disSid:this.disId,
+					tradeType : this.tradeType,
+					lowThedate: this.lowThedate,
+					highThedate:this.highThedate,
+					sort:this.sortBy,
+					sortColumn:this.sortColumn,
+					begin:this.begin||0,
+					end:this.end||this.tableLength-1
+				}
+			}).then(function(resp){
+				if(resp.success){
+					self.keyword.list=resp.value.list;
+					if(callback){
+						callback(resp.value);
+					}
+				}else{
+					alert(resp.message);
+				}
+			});
+		};
+		this.getKeyWordDetail = function(query,callback){
+			var getAjax = tools.promise(api.getDistributorDetailQueryEffectsByQuery,true);
+			getAjax({
+				data:{
+					disSid:this.disId,
+					tradeType : this.tradeType,
+					lowThedate: this.lowThedate,
+					highThedate:this.highThedate,
+					query:query
+				}
+			}).then(function(resp){
+				if(resp.success){
+					self.keyword.detail=resp.value;
+					if(callback){
+						callback(resp.value);
+					}
+				}else{
+					alert(resp.message);
+				}
+			});
+		};
+		/*流量渠道*/
+		this.getDistriDataSrc = function(callback){
+			var getAjax = tools.promise(api.getDistributorDetailPvSrcEffectsByShop,true);
+			getAjax({
+				data:{
+					disSid:this.disId,
+					tradeType : this.tradeType,
+					lowThedate: this.lowThedate,
+					highThedate:this.highThedate,
+					sort:this.sortBy,
+					sortColumn:this.sortKey,
+					begin:this.begin||0,
+					end:this.end||this.tableLength-1
+				}
+			}).then(function(resp){
+				if(resp.success){
+					//成功
+					console.log(resp);
+					var _arry = _.filter(resp.value,function(item){return item.srcLevel<=2});//清除2级目录下的其他东东..
+                    var pil = _.indexBy(_arry,function(item){return item.srcParentId===0});
+					var _arryTmp = _.filter(_arry,function(item){return item.srcParentId===0});//进行循环的数组
+					var _src = [];
+					//debugger;
+					_.each(_arryTmp,function(item,index){
+						item.chanels = _.filter(_arry,function(i){
+							return i.srcParentId === item.srcId
+						});
+						var length = item.chanels.length;
+						if(length){
+							item.chanels[0].parentName=item.srcName;
+							item.chanels[0].childLength = length;
+						}
+						//debugger;
+						_src = _src.concat(item.chanels);
+					});
+					self.distriDataSrc = _src;
+					if(callback){
+						callback(resp.value);
+					}
+				}
+			});
+		}
+		this.getProds = function(callback){
+			//获取授权产品及折扣
+			var getAjax = tools.promise(api.getPrudsList,true);
+			getAjax({
+				data:{
+					disSid:this.disId,
+					begin:this.begin||0,
+					count:this.tableLength
+				}
+			}).then(function(resp){
+				if(resp.success){
+					//debugger;
+					console.log(resp.value);
+					self.productLines =  _.indexBy(resp.value.productLines,'productLineId');
+					self.prods = resp.value.list;
+					if(callback){
+						callback(resp.value);
+					}
+				}else{
+					alert(resp.message);
+				}
+			});
+		}
+		this.editCount = function(index,value){
+			if(value==''){
+				alert('请输入整数');
+				return;
+			};
+			value = Number(value);
+			if(isNaN(value)){
+				alert('请输入整数');
+				return;
+			};
+			value = value.toFixed(2);
+			var prod = this.prods[index];
+			var price = (prod.standardPrice*(value/100)).toFixed(2);
+ 			tools.http({
+				url:api.updatePrice,
+				data:{
+					pids:prod.pid,
+					disId:item.disId,
+					disSid:this.disId,
+					prices:price
+				},
+				succ:function(resp){
+					if(resp.success){
+						alert('修改成功');
+						prod.agentCostPrice = price;
+					}else{
+						alert(resp.message);
+					}
+				}
+			});
+		};
+		//获取联系记录
+		this.getRecordList = function(callback){
+			var getAjax = tools.promise(api.getRecordList,true);
+			getAjax({
+				data:{
+					disSid:this.disId
+				}
+			}).then(function(resp){
+				if(resp.success){
+					//
+					self.concats = resp.value.disLinkManInfos;
+					if(callback){
+						callback(resp.value);
+					}
+				}
+			});
+		};
+		//添加联系人
+		this.addConcat = function(callback){
+			var item = self.concats[0];
+			if(item.id){
+				//修改
+				return;
+			};
+			$.extend(item,{disSid:this.disId});
+			tools.http({
+				url:api.addConcat,
+				data:item,
+				succ:function(resp){
+					if(resp.success){
+						item.id =  resp.value.id;
+					}else{
+						alert(resp.message);
+					}
+				}
+			});
+		};
+		//修改联系人
+		this.alterConcat = function(item){
+			if(!item.id){
+				alert('页面异常，请刷新页面');
+				return;
+			}
+			tools.http({
+				url:api.updateConcat,
+				data:item,
+				succ:function(resp){
+					if(resp.success){
+						alert('修改成功');
+					}else{
+						alert('修改失败，请刷新页面重试');
+					}
+				}
+			});
+		};
+		this.deleteConcat = function(item){
+			if(!item.id){
+				//alert('页面异常，请刷新页面');
+				self.concats.shift();
+				return;
+			};
+			tools.http({
+				url:api.deleteConcat,
+				data:{
+					id:item.id
+				},
+				succ:function(resp){
+					if(resp.success){
+						alert('删除成功..');
+						debugger;
+						self.concats =_.reject(self.concats,function(i){
+							return i.id==item.id
+						});
+					}
+				}
+			});
 		}
 		return this;
 	}
 	return action;
+}]);
+
+
+dm.factory('initPages',['tools',function(tools){
+	return function($scope,length,count){
+		var maxPages = 0,pages=[];
+		var count = count || tools.config.table.count;
+		if(length>=count){
+			maxPages = length%count==0?(length/count):(length/count+1);	
+		}
+		for(var i=0,j=Math.floor(maxPages);i<j&&j!=1;i++){
+			pages.push(i);
+		}
+		$scope.pages=pages;
+		$scope.now=1;
+	}
 }]);
