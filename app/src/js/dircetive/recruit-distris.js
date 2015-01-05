52d1c80d5159ae0f1095ed5f9533eef9c8226728
korealiang <hanguoliang1992@gmail.com>
2015-01-05 10:58:45 +0800

    2015-1-5

--- app/src/js/dircetive/recruit-distris.js
+++ app/src/js/dircetive/recruit-distris.js
@@ -5,7 +5,7 @@ dm.directive('recruitDistris',['$parse',function($parse){
 		}
 	};
 	var controller = ['$scope','$element','$attrs','initPages','recruitDistrisModel',function($scope,$element,$attrs,initPages,recruitDistrisModel){
-		debugger;
+		//debugger;
 		var tagIndex = $scope.$parent.tagIndex,status,isSimilarQ;
 		console.log(tagIndex);
 		switch(Number(tagIndex)){
@@ -31,11 +31,75 @@ dm.directive('recruitDistris',['$parse',function($parse){
 		model.getData(null,function(v){
 			initPages($scope,v.totalSize);
 		});
+		//获取分页
 		$scope.getPage = function(page){
-			alert(page);
+			//alert(page);
+			if($scope.now == page){
+				return;
+			}
+			$scope.now=page;
+			var count = $scope.model.count,
+				begin = (page-1)*count,
+				end = page*count-1;
+			$scope.getData({
+				begin:begin
+			});
 		};
+		//获取排序
 		$scope.getSort = function(e,item){
-
+		};
+		//获取查询
+		$scope.getSearch = function(){
+			//todo: 所在地数据获取 
+			var lowOperateTime  = $('.low',$element).val();//操作时间
+			var highOperateTime  = $('.high',$element).val();
+			var lowShopCreateTime = $('.creat-time-low',$element).val();//开店时间
+			var highShopCreateTime = $('.creat-time-high',$element).val();
+			//好评率
+			var lowBuyerGoodRate ='',highBuyerGoodRate='';
+			var _lowBuyerGoodRate = Number($('.low-buyer-rate',$element).val()||'s');
+			if(!isNaN(_lowBuyerGoodRate)){
+				lowBuyerGoodRate = (_lowBuyerGoodRate/100).toFixed(2);
+			}
+			var _highBuyerGoodRate = Number($('.high-buyer-rate',$element).val()||'s');
+			if(!isNaN(_highBuyerGoodRate)){
+				highBuyerGoodRate = (_highBuyerGoodRate/100).toFixed(2);
+			}
+			model.getData({
+				begin:0,
+				lowOperateTime:lowOperateTime,
+				highOperateTime:highOperateTime,
+				lowShopCreateTime:lowShopCreateTime,
+				highShopCreateTime:highShopCreateTime,
+				lowBuyerGoodRate:lowBuyerGoodRate,
+				highBuyerGoodRate:highBuyerGoodRate
+			},function(v){
+				initPages($scope,v.totalSize);
+			});
+		};
+		//清除筛选
+		$scope.getRest = function(){
+			$('.low',$element).val('');
+			$('.high',$element).val('');
+			$('.creat-time-low',$element).val('');
+			$('.creat-time-high',$element).val('');
+			$('.low-buyer-rate',$element).val('');
+			$('.high-buyer-rate',$element).val('');
+			var oldPrams = $scope.model.parms;
+			var tmpObj = {
+				status:oldPrams.status,
+				isSimilarQ:oldPrams.isSimilarQ,
+				begin:0,
+				count:oldPrams.count
+			};
+			$scope.model.parms = tmpObj;
+		};
+		//全选
+		$scope.selectAll = function(e){
+			//debugger
+			$label = $(e.currentTarget);
+			$check = $label.find('input[type="checkbox"]');
+			model.selectAll($check.prop('checked'));
 		};
 	}];
 	return {
@@ -50,7 +114,8 @@ dm.directive('recruitDistris',['$parse',function($parse){
 dm.factory('recruitDistrisModel',['tools',function(tools){
 	var api = {
 		getList:'getRecruitOfDistributors.htm',
-		getOperaters:'getRecruitOfOperaters.htm'
+		getOperaters:'getRecruitOfOperaters.htm',
+		updateStatus:'moveRecruitOfDistributors.htm'
 	};
 	var action = function(status,isSimilarQ){
 		var self= this;
@@ -62,6 +127,7 @@ dm.factory('recruitDistrisModel',['tools',function(tools){
 			count:this.count
 		};
 		this.graders = tools.shopLevels;
+		//获取数据
 		this.getData = function(parm,callback){
 			$.extend(this.parms,parm);
 			var getAjax = tools.promise(api.getList,true);
@@ -81,8 +147,13 @@ dm.factory('recruitDistrisModel',['tools',function(tools){
 				}
 			});
 		};
+		//全选
 		this.selectAll = function(bool){
+			_.each(this.list,function(item,index){
+				item.select = bool;
+			});
 		};
+		//获取操作员
 		this.getOperaters = function(callback){
 			var getAjax = tools.promise(api.getOperaters,true);
 			getAjax().then(function(resp){
@@ -91,6 +162,40 @@ dm.factory('recruitDistrisModel',['tools',function(tools){
 				}
 			});
 		};
+		//状态操作
+		this.updateStatus = function(){
+			console.log(this.Status);
+			var sids=[],items=[];
+			if(!this.Status){
+				alert('请选择要移动的状态');
+				return;
+			}
+			_.each(this.list,function(item){
+				if(item.select){
+					sids.push(item.sid);
+					items.push(item);
+				}
+			});
+			if(!sids.length){
+				alert('请选择要进行操作的分销商');
+				return;
+			}
+			tools.http({
+				url:api.updateStatus,
+				data:{
+					sids:sids.join(','),
+					status:this.Status
+				},
+				succ:function(resp){
+					if(resp.success){
+						alert('修改成功');
+						_.each(items,function(item){
+							item.status = this.updateStatus;
+						});
+					}
+				}
+			});
+		}
 		return this;
 	};
 	return action;
